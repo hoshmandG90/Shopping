a1c3f3be558dbdbd9158b4c644a0c63bef127134
HoshmandG90 <hoshmandG900@gmail.com>
2022-03-17 17:11:13 +0300

    updated some pages

--- app/Http/Controllers/Clothes/Details.php
+++ app/Http/Controllers/Clothes/Details.php
@@ -4,15 +4,40 @@ namespace App\Http\Controllers\Clothes;
 
 use Livewire\Component;
 use App\Models\clothes;
-
+use App\Models\Sales;
 class Details extends Component
 {
 
     public $clothes;
 
+    public $quantity;
+
     public function mount($id)
     {
         $this->clothes = clothes::find($id);
+    }
+
+    public function addToCart()
+    {
+        if($this->quantity){
+            Sales::create([
+                'title' => $this->clothes->title,
+                'excerpt' => $this->clothes->excerpt,
+                'price' => $this->clothes->price,
+                'quantity' => $this->quantity,
+                'location' => $this->clothes->location,
+                'brand' => $this->clothes->brand,
+            ]);
+            notyf()->livewire()->position('y','top')->addSuccess("Your item has been added to cart");
+            return redirect()->back();
+
+
+        }else{
+            notyf()->livewire()->position('y','top')->addError("Please enter quantity");
+            return redirect()->back();
+        }
+       
+
     }
 
     public function render()
--- app/Http/Controllers/Electronic/Details.php
+++ app/Http/Controllers/Electronic/Details.php
@@ -4,14 +4,38 @@ namespace App\Http\Controllers\Electronic;
 
 use Livewire\Component;
 use App\Models\electronics;
-
+use App\Models\Sales;
 class Details extends Component{
 
     public $electronic;
 
+    public $quantity;
     public function mount($id){
         $this->electronic=electronics::find($id);
     }
+
+    public function addToCart()
+    {
+        if($this->quantity){
+            Sales::create([
+                'title' => $this->electronic->title,
+                'excerpt' => $this->electronic->excerpt,
+                'price' => $this->electronic->price,
+                'quantity' => $this->quantity,
+                'location' => $this->electronic->location,
+                'brand' => $this->electronic->brand,
+            ]);
+            notyf()->livewire()->position('y','top')->addSuccess("Your item has been added to cart");
+            return redirect()->back();
+
+
+        }else{
+            notyf()->livewire()->position('y','top')->addError("Please enter quantity");
+            return redirect()->back();
+        }
+    }
+       
+
     public function render()
     {
         return view('electronic.details')->extends('layouts.master');
